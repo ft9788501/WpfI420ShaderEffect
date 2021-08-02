fdb6970d07dc396b349ef98256ffce42b5c613cf
jim.jiang@ringcentral.com <jim.jiang@ringcentral.com>
2021-08-02 15:22:40 +0800

    first upload

---
+++ WpfI420ShaderEffect/WpfI420ShaderEffect/I420ShaderEffect.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace WpfI420ShaderEffect
+{
+    public class I420ShaderEffect : ShaderEffect
+    {
+        #region DependencyProperty
+
+        public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty("Input", typeof(I420ShaderEffect), 0);
+        public static readonly DependencyProperty TextureYProperty = RegisterPixelShaderSamplerProperty("TextureY", typeof(I420ShaderEffect), 1);
+        public static readonly DependencyProperty TextureUProperty = RegisterPixelShaderSamplerProperty("TextureU", typeof(I420ShaderEffect), 2);
+        public static readonly DependencyProperty TextureVProperty = RegisterPixelShaderSamplerProperty("TextureV", typeof(I420ShaderEffect), 3);
+
+        public Brush Input
+        {
+            get => (Brush)GetValue(InputProperty);
+            set => SetValue(InputProperty, value);
+        }
+        public ImageBrush TextureY
+        {
+            get => (ImageBrush)GetValue(TextureYProperty);
+            set => SetValue(TextureYProperty, value);
+        }
+        public ImageBrush TextureU
+        {
+            get => (ImageBrush)GetValue(TextureUProperty);
+            set => SetValue(TextureUProperty, value);
+        }
+        public ImageBrush TextureV
+        {
+            get => (ImageBrush)GetValue(TextureVProperty);
+            set => SetValue(TextureVProperty, value);
+        }
+
+        #endregion
+
+        [DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory")]
+        public static extern void CopyMemory(IntPtr Destination, IntPtr Source, int Length);
+
+        DateTime lastDateTime = DateTime.Now;
+
+        static int textureWidth = 2200;
+        static int textureHeight = 1200;
+        static int videoWidth = 1920;
+        static int videoHeight = 1080;
+        YuvLoader yuvLoader = YuvLoader.LoadFromPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test1080.yuv"), videoWidth, videoHeight);
+        WriteableBitmap writeableBitmapY = new WriteableBitmap(textureWidth, textureHeight, 96, 96, PixelFormats.Gray8, null);
+        WriteableBitmap writeableBitmapU = new WriteableBitmap(textureWidth / 2, textureHeight / 2, 96, 96, PixelFormats.Gray8, null);
+        WriteableBitmap writeableBitmapV = new WriteableBitmap(textureWidth / 2, textureHeight / 2, 96, 96, PixelFormats.Gray8, null);
+        IntPtr backBufferY;
+        IntPtr backBufferU;
+        IntPtr backBufferV;
+
+        public I420ShaderEffect()
+        {
+            PixelShader = new()
+            {
+                UriSource = new Uri(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "I420ShaderEffect.ps"), UriKind.Absolute)
+            };
+
+            backBufferY = writeableBitmapY.BackBuffer;
+            backBufferU = writeableBitmapU.BackBuffer;
+            backBufferV = writeableBitmapV.BackBuffer;
+
+            TextureY = new ImageBrush(writeableBitmapY);
+            TextureU = new ImageBrush(writeableBitmapU);
+            TextureV = new ImageBrush(writeableBitmapV);
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(25)
+            };
+
+            yuvLoader.YuvFrameDataReceive((y, u, v, w, h) =>
+            {
+                if ((DateTime.Now - lastDateTime).TotalMilliseconds < 25)
+                {
+                    return;
+                }
+                lastDateTime = DateTime.Now;
+                unsafe
+                {
+                    for (int i = 0; i < videoHeight; i++)
+                    {
+                        IntPtr newVPoint = new(y.ToInt64() + i * videoWidth);
+                        IntPtr newPoint = new(backBufferY.ToInt64() + i * textureWidth);
+                        CopyMemory(newPoint, newVPoint, videoWidth);
+                    }
+                    for (int i = 0; i < videoHeight / 2; i++)
+                    {
+                        IntPtr newVPoint = new(u.ToInt64() + i * videoWidth / 2);
+                        IntPtr newPoint = new(backBufferU.ToInt64() + i * textureWidth / 2);
+                        CopyMemory(newPoint, newVPoint, videoWidth / 2);
+                    }
+                    for (int i = 0; i < videoHeight / 2; i++)
+                    {
+                        IntPtr newVPoint = new(v.ToInt64() + i * videoWidth / 2);
+                        IntPtr newPoint = new(backBufferV.ToInt64() + i * textureWidth / 2);
+                        CopyMemory(newPoint, newVPoint, videoWidth / 2);
+                    }
+                }
+            });
+            dispatcherTimer.Tick += (s, e) =>
+            {
+                writeableBitmapY.Lock();
+                writeableBitmapY.AddDirtyRect(new Int32Rect(0, 0, textureWidth, textureHeight));
+                writeableBitmapY.Unlock();
+                writeableBitmapU.Lock();
+                writeableBitmapU.AddDirtyRect(new Int32Rect(0, 0, textureWidth / 2, textureHeight / 2));
+                writeableBitmapU.Unlock();
+                writeableBitmapV.Lock();
+                writeableBitmapV.AddDirtyRect(new Int32Rect(0, 0, textureWidth / 2, textureHeight / 2));
+                writeableBitmapV.Unlock();
+            };
+            dispatcherTimer.Start();
+        }
+    }
+}
---
+++ WpfI420ShaderEffect/WpfI420ShaderEffect/YuvLoader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfI420ShaderEffect
+{
+    public class YuvLoader : IDisposable
+    {
+        private class YuvFrameData
+        {
+            public byte[] Y { get; set; }
+            public byte[] U { get; set; }
+            public byte[] V { get; set; }
+        }
+
+        public static YuvLoader LoadFromPath(string yuvFilePath, int width, int height)
+        {
+            if (File.Exists(yuvFilePath))
+            {
+                return new YuvLoader(yuvFilePath, width, height);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private readonly FileStream fileStream;
+        private YuvFrameData[] YuvFrameDatas { get; }
+
+        public int FrameCount { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        private YuvLoader(string yuvFilePath, int width, int height)
+        {
+            fileStream = new FileStream(yuvFilePath, FileMode.Open);
+            Width = width;
+            Height = height;
+            FrameCount = (int)(fileStream.Length / (width * height * 1.5));
+            YuvFrameDatas = new YuvFrameData[FrameCount];
+            int index = 0;
+            while (fileStream.Position != fileStream.Length)
+            {
+                var yBuffer = new byte[Width * Height];
+                var uBuffer = new byte[Width * Height / 4];
+                var vBuffer = new byte[Width * Height / 4];
+                fileStream.Read(yBuffer, 0, yBuffer.Length);
+                fileStream.Read(uBuffer, 0, uBuffer.Length);
+                fileStream.Read(vBuffer, 0, vBuffer.Length);
+
+                var yuvFrameData = new YuvFrameData
+                {
+                    Y = yBuffer,
+                    U = uBuffer,
+                    V = vBuffer
+                };
+                YuvFrameDatas[index++] = yuvFrameData;
+            }
+        }
+        public void YuvFrameDataReceive(Action<IntPtr, IntPtr, IntPtr, int, int> callback)
+        {
+            int index = 0;
+            int interval = 25;
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    if (index == FrameCount)
+                    {
+                        index = 0;
+                    }
+                    var d = YuvFrameDatas[index++];
+                    byte[] y = new byte[d.Y.Length];
+                    byte[] u = new byte[d.U.Length];
+                    byte[] v = new byte[d.V.Length];
+                    Buffer.BlockCopy(d.Y, 0, y, 0, d.Y.Length);
+                    Buffer.BlockCopy(d.U, 0, u, 0, d.U.Length);
+                    Buffer.BlockCopy(d.V, 0, v, 0, d.V.Length);
+
+                    callback?.Invoke(Marshal.UnsafeAddrOfPinnedArrayElement(y, 0), Marshal.UnsafeAddrOfPinnedArrayElement(u, 0), Marshal.UnsafeAddrOfPinnedArrayElement(v, 0), Width, Height);
+                    Thread.Sleep(interval);
+                }
+            })
+            {
+                IsBackground = true
+            }.Start();
+        }
+
+        public void Dispose()
+        {
+            fileStream?.Dispose();
+        }
+    }
+}
